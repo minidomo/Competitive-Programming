02208c6a0eb050e395b4dbbe0edda817111e2b7f
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2019-03-11 16:15:16 -0500

    my solutions

---
+++ A+ CS/2019/Packet 3/yahtzeeodds.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class yahtzeeodds {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String[] arr = sc.nextLine().split("[,\\s]");
+            int count = 0;
+            String s = arr[5].substring(1, arr[5].length() - 1);
+            for (int x = 0; x < arr.length - 1; x++)
+                if (!arr[x].equals(s))
+                    count++;
+            System.out.println("1 in " + (int) Math.pow(6, count));
+        }
+        sc.close();
+    }
+}
