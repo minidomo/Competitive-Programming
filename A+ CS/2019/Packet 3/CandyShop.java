87729f54e5038aefa409f302ed3bc9ec5a9a509b
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2019-03-11 16:26:13 -0500

    adjusted file and class names

---
+++ A+ CS/2019/Packet 3/CandyShop.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class CandyShop {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int[] val = { 3, 5, 50, 100, 10 };
+        while (sc.hasNextLine()) {
+            int a = (int) (100 * Double.parseDouble(sc.nextLine().substring(1).trim()));
+            int[] dp = new int[a + 1];
+            dp[0] = 1;
+            for (int x = 0; x < 5; x++)
+                for (int s = 0; s <= a; s++)
+                    if (s + val[x] <= a)
+                        dp[s + val[x]] += dp[s];
+            System.out.println(dp[a]);
+        }
+        sc.close();
+    }
+}
